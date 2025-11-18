33087765102e8ad8304fd324ea60d5055e025d3f
Djeff <117030562+jEFFLEZ@users.noreply.github.com>
2025-11-18 05:50:35 +0100

    feat(license): gumroad license activation endpoints and CLI; add env vars

---
+++ src/commands/license-status.ts
@@ -0,0 +1,11 @@
+import license from '../utils/license';
+
+export default async function runLicenseStatus() {
+  const lic = license.readLicense();
+  if (!lic) {
+    console.log('No license found');
+    return 0;
+  }
+  console.log(JSON.stringify(lic, null, 2));
+  return 0;
+}
